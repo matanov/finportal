b81e6efbae2b42b90daa02b47e82d2b4687bc08e
Panto Matanov <matanov@hotmail.com>
2026-07-12 18:05:43 -0400

    feat: add custom domain fersmath.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://matanov.github.io',
-  base: 'finportal',
+  site: 'https://fersmath.com',
   vite: {
     plugins: [tailwindcss()],
   },
